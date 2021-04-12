6adaefd8fdb1650001ba96213094bea50f69901c
MAURICIOV2 <82234135+MAURICIOV2@users.noreply.github.com>
2021-04-12 17:39:32 -0500

    Nueva

--- vistas/modulos/navlateral.php
+++ vistas/modulos/navlateral.php
@@ -1,4 +1,128 @@
-¿
+
+<div id="sidebar-menu" class="main_menu_side hidden-print main_menu">
+    <div class="menu_section">
+        <h3>General</h3>
+        <ul class="nav side-menu">
+            <li><a><i class="fa fa-home"></i> Tablero <span class="fa fa-chevron-down"></span></a>
+                <ul class="nav child_menu">
+                    <li><a href="<?php echo SERVERURL; ?>home/">Tablero</a></li>
+                    <!--<li><a href="<?php echo SERVERURL; ?>personal/">Informacación personal</a></li>-->
+                    <li><a href="<?php echo SERVERURL; ?>perfil/">Información cuenta</a></li>
+                </ul>
+            </li>
+
+
+
+                    <li><a><i class="fa fa-institution"></i> Mundo Jeans <span class="fa fa-chevron-down"></span></a>
+                        <ul class="nav child_menu">
+                            <li><a href="<?php echo SERVERURL; ?>municipio/">Empresa</a></li>
+                        </ul>
+                    </li>
+                 
+
+            </ul>
+
+
+<br>
+
+            <h3>Modulos Contabilidad</h3>
+            <br>
+        <ul class="nav side-menu">
+       
+                    
+                    <li><a><i class="fa fa-users"></i> Plan de cuentas <span class="fa fa-chevron-down"></span></a>
+                        <ul class="nav child_menu">
+                            <li><a href="<?php echo SERVERURL; ?>plancuentas/">Plan de cuentas</a></li>
+                        </ul>
+                    </li>
+
+                    <li><a><i class="fa fa-users"></i> Libro diario <span class="fa fa-chevron-down"></span></a>
+                        <ul class="nav child_menu">
+                            <li><a href="<?php echo SERVERURL; ?>librodiario/">Libro diario</a></li>
+                        </ul>
+                    </li>
+
+                    <li><a><i class="fa fa-users"></i> Mayores generales <span class="fa fa-chevron-down"></span></a>
+                        <ul class="nav child_menu">
+                            <li><a href="<?php echo SERVERURL; ?>mayoresgenerales/">Mayores generales</a></li>
+                        </ul>
+                    </li>
+
+                    <li><a><i class="fa fa-users"></i> Bancos <span class="fa fa-chevron-down"></span></a>
+                        <ul class="nav child_menu">
+                            <li><a href="<?php echo SERVERURL; ?>consiliacionbancaria/">Consiliación Bancaria</a></li>
+                        </ul>
+                    </li>
+
+                    <li><a><i class="fa fa-users"></i> Estados financieros <span class="fa fa-chevron-down"></span></a>
+                        <ul class="nav child_menu">
+                            <li><a href="<?php echo SERVERURL; ?>balancefinanciero/">Balance de situación financiera</a></li>
+                            <li><a href="<?php echo SERVERURL; ?>balanceresultados/">Balance de resultados</a></li>
+                        </ul>
+                    </li>
+
+                    <li><a><i class="fa fa-users"></i> Personal <span class="fa fa-chevron-down"></span></a>
+                        <ul class="nav child_menu">
+                            <li><a href="<?php echo SERVERURL; ?>empleados/">Empleados</a></li>
+                            <li><a href="<?php echo SERVERURL; ?>estadosEmpleado/">Estados empleado</a></li>
+                        </ul>
+                    </li>
+
+ 
+
+                   
+ 
+                    
+
+                   <li><a><i class="fa fa-user"></i> Administradores <span class="fa fa-chevron-down"></span></a>
+                    <ul class="nav child_menu">
+                        <li><a href="<?php echo SERVERURL; ?>administrador/">Administrador</a></li>
+                        <!--<li><a href="<?php echo SERVERURL; ?>perfil/">Perfil de usuario</a></li>-->
+                        <li><a href="<?php echo SERVERURL; ?>estadosAdministrador/">Estados del administrador</a></li>
+                    </ul>
+                </li>
+
+                <li><a><i class="fa fa-windows"></i> Accesos <span class="fa fa-chevron-down"></span></a>
+                    <ul class="nav child_menu">
+                        <li><a href="<?php echo SERVERURL; ?>roles/">Roles</a></li>
+                        <li><a href="<?php echo SERVERURL; ?>modulos/">Modulos</a></li>
+                    </ul>
+                </li>
+
+            </ul>
+
+
+
+    </div>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- vistas/plantilla.php
+++ vistas/plantilla.php
@@ -85,7 +85,7 @@ else:
                 <div class="col-md-3 left_col">
                     <div class="left_col scroll-view">
                         <div class="navbar nav_title" style="border: 0;">
-                            <a href="<?php echo SERVERURL; ?>home/"" class="site_title"><i class="fa fa-laptop"></i> <span>Mantenimiento!</span></a>
+                            <a href="<?php echo SERVERURL; ?>home/"" class="site_title"><i class="fa fa-laptop"></i> <span>Mundo Jeans!</span></a>
                         </div>
 
                         <div class="clearfix"></div>
