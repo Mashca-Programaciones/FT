ef8558f1615d5560a2b549a510230b304bc7b583
MAURICIOV2 <82234135+MAURICIOV2@users.noreply.github.com>
2021-04-12 20:36:26 -0500

    Nuevo

--- vistas/modulos/navlateral.php
+++ vistas/modulos/navlateral.php
@@ -13,95 +13,114 @@
 
 
 
-                    <li><a><i class="fa fa-institution"></i> Mundo Jeans <span class="fa fa-chevron-down"></span></a>
-                        <ul class="nav child_menu">
-                            <li><a href="<?php echo SERVERURL; ?>municipio/">Empresa</a></li>
-                        </ul>
-                    </li>
-                 
+            <li><a><i class="fa fa-institution"></i> Mundo Jeans <span class="fa fa-chevron-down"></span></a>
+                <ul class="nav child_menu">
+                    <li><a href="<?php echo SERVERURL; ?>municipio/">Empresa</a></li>
+                    <li><a href="<?php echo SERVERURL; ?>administrador/">Administrador</a></li>
+                    <li><a href="<?php echo SERVERURL; ?>roles/">Roles</a></li>
+                    <li><a href="<?php echo SERVERURL; ?>modulos/">Modulos</a></li>
+                </ul>
+            </li>
 
-            </ul>
+
+        </ul>
 
 
 
-            <br>
+        <!--<br>
 
-            <h3>Ventas</h3>
-   
+        <h3>Ventas</h3>-->
+
         <ul class="nav side-menu">
-       
-                    
-                    <li><a><i class="fa fa-users"></i> Inventario <span class="fa fa-chevron-down"></span></a>
-                        <ul class="nav child_menu">
-                            <li><a href="<?php echo SERVERURL; ?>plancuentas/">Facturación</a></li>
-                            <li><a href="<?php echo SERVERURL; ?>plancuentas/">Retenciones</a></li>
-                            <li><a href="<?php echo SERVERURL; ?>plancuentas/">Clientes</a></li>
-                        </ul>
-                    </li>
-            </ul>
 
 
+            <li><a><i class="fa fa-users"></i> Ventas <span class="fa fa-chevron-down"></span></a>
+                <ul class="nav child_menu">
+                    <li><a href="<?php echo SERVERURL; ?>plancuentas/">Facturación</a></li>
+                    <li><a href="<?php echo SERVERURL; ?>plancuentas/">Retenciones</a></li>
+                    <li><a href="<?php echo SERVERURL; ?>plancuentas/">Clientes</a></li>
+                </ul>
+            </li>
+        </ul>
 
-<br>
+        <!--<br>
 
-            <h3>Inventario</h3>
+        <h3>Compras</h3>-->
+        
+        <ul class="nav side-menu">
+         
             
-        <ul class="nav side-menu">
-       
-                    
-                    <li><a><i class="fa fa-users"></i> Inventario <span class="fa fa-chevron-down"></span></a>
-                        <ul class="nav child_menu">
-                            <li><a href="<?php echo SERVERURL; ?>plancuentas/">Articulo</a></li>
-                            <li><a href="<?php echo SERVERURL; ?>plancuentas/">Cardex</a></li>
-                        </ul>
-                    </li>
-            </ul>
+            <li><a><i class="fa fa-users"></i> Compras <span class="fa fa-chevron-down"></span></a>
+                <ul class="nav child_menu">
+                    <li><a href="<?php echo SERVERURL; ?>plancuentas/">Facturación</a></li>
+                    <li><a href="<?php echo SERVERURL; ?>plancuentas/">Retenciones</a></li>
+                    <li><a href="<?php echo SERVERURL; ?>plancuentas/">Clientes</a></li>
+                </ul>
+            </li>
+        </ul>
 
 
 
+        <!--<br>
 
-
-            <br>
-
-            <h3>Modulos Contabilidad</h3>
+        <h3>Inventario</h3>-->
 
         <ul class="nav side-menu">
-       
-                    
-                    <li><a><i class="fa fa-users"></i> Plan de cuentas <span class="fa fa-chevron-down"></span></a>
-                        <ul class="nav child_menu">
-                            <li><a href="<?php echo SERVERURL; ?>plancuentas/">Plan de cuentas</a></li>
-                        </ul>
-                    </li>
 
-                    <li><a><i class="fa fa-users"></i> Libro diario <span class="fa fa-chevron-down"></span></a>
-                        <ul class="nav child_menu">
-                            <li><a href="<?php echo SERVERURL; ?>librodiario/">Libro diario</a></li>
-                        </ul>
-                    </li>
 
-                    <li><a><i class="fa fa-users"></i> Mayores generales <span class="fa fa-chevron-down"></span></a>
-                        <ul class="nav child_menu">
-                            <li><a href="<?php echo SERVERURL; ?>mayoresgenerales/">Mayores generales</a></li>
-                        </ul>
-                    </li>
+            <li><a><i class="fa fa-users"></i> Inventario <span class="fa fa-chevron-down"></span></a>
+                <ul class="nav child_menu">
+                    <li><a href="<?php echo SERVERURL; ?>plancuentas/">Articulo</a></li>
+                    <li><a href="<?php echo SERVERURL; ?>plancuentas/">Cardex</a></li>
+                </ul>
+            </li>
+        </ul>
 
-                    <li><a><i class="fa fa-users"></i> Bancos <span class="fa fa-chevron-down"></span></a>
-                        <ul class="nav child_menu">
-                            <li><a href="<?php echo SERVERURL; ?>consiliacionbancaria/">Consiliación Bancaria</a></li>
-                        </ul>
-                    </li>
 
-                    <li><a><i class="fa fa-users"></i> Estados financieros <span class="fa fa-chevron-down"></span></a>
-                        <ul class="nav child_menu">
-                            <li><a href="<?php echo SERVERURL; ?>balancefinanciero/">Balance de situación financiera</a></li>
-                            <li><a href="<?php echo SERVERURL; ?>balanceresultados/">Balance de resultados</a></li>
-                        </ul>
-                    </li>
 
-                   
 
-            </ul>
+
+        <br>
+
+        <h3>Modulos Contabilidad</h3>
+
+        <ul class="nav side-menu">
+
+
+            <li><a><i class="fa fa-users"></i> Plan de cuentas <span class="fa fa-chevron-down"></span></a>
+                <ul class="nav child_menu">
+                    <li><a href="<?php echo SERVERURL; ?>plancuentas/">Plan de cuentas</a></li>
+                </ul>
+            </li>
+
+            <li><a><i class="fa fa-users"></i> Libro diario <span class="fa fa-chevron-down"></span></a>
+                <ul class="nav child_menu">
+                    <li><a href="<?php echo SERVERURL; ?>librodiario/">Libro diario</a></li>
+                </ul>
+            </li>
+
+            <li><a><i class="fa fa-users"></i> Mayores generales <span class="fa fa-chevron-down"></span></a>
+                <ul class="nav child_menu">
+                    <li><a href="<?php echo SERVERURL; ?>mayoresgenerales/">Mayores generales</a></li>
+                </ul>
+            </li>
+
+            <li><a><i class="fa fa-users"></i> Bancos <span class="fa fa-chevron-down"></span></a>
+                <ul class="nav child_menu">
+                    <li><a href="<?php echo SERVERURL; ?>consiliacionbancaria/">Consiliación Bancaria</a></li>
+                </ul>
+            </li>
+
+            <li><a><i class="fa fa-users"></i> Estados financieros <span class="fa fa-chevron-down"></span></a>
+                <ul class="nav child_menu">
+                    <li><a href="<?php echo SERVERURL; ?>balancefinanciero/">Balance de situación financiera</a></li>
+                    <li><a href="<?php echo SERVERURL; ?>balanceresultados/">Balance de resultados</a></li>
+                </ul>
+            </li>
+
+
+
+        </ul>
 
 
 
@@ -153,103 +172,103 @@
 
 
 
-                    <li><a><i class="fa fa-institution"></i> Empresa <span class="fa fa-chevron-down"></span></a>
-                        <ul class="nav child_menu">
-                            <li><a href="<?php echo SERVERURL; ?>municipio/">Empresa</a></li>
-                            <li><a href="<?php echo SERVERURL; ?>departamentos/">Departamentos</a></li>
-                            <li><a href="<?php echo SERVERURL; ?>cargos/">Cargos</a></li>
-                        </ul>
-                    </li>
- 
-                    <li><a><i class="fa fa-users"></i> Personal <span class="fa fa-chevron-down"></span></a>
-                        <ul class="nav child_menu">
-                            <li><a href="<?php echo SERVERURL; ?>empleados/">Empleados</a></li>
-                            <li><a href="<?php echo SERVERURL; ?>estadosEmpleado/">Estados empleado</a></li>
-                        </ul>
-                    </li>
+            <li><a><i class="fa fa-institution"></i> Empresa <span class="fa fa-chevron-down"></span></a>
+                <ul class="nav child_menu">
+                    <li><a href="<?php echo SERVERURL; ?>municipio/">Empresa</a></li>
+                    <li><a href="<?php echo SERVERURL; ?>departamentos/">Departamentos</a></li>
+                    <li><a href="<?php echo SERVERURL; ?>cargos/">Cargos</a></li>
+                </ul>
+            </li>
 
-                    <li><a><i class="fa fa-sign-in"></i> Ingresos <span class="fa fa-chevron-down"></span></a>
+            <li><a><i class="fa fa-users"></i> Personal <span class="fa fa-chevron-down"></span></a>
+                <ul class="nav child_menu">
+                    <li><a href="<?php echo SERVERURL; ?>empleados/">Empleados</a></li>
+                    <li><a href="<?php echo SERVERURL; ?>estadosEmpleado/">Estados empleado</a></li>
+                </ul>
+            </li>
+
+            <li><a><i class="fa fa-sign-in"></i> Ingresos <span class="fa fa-chevron-down"></span></a>
+                <ul class="nav child_menu">
+                    <li><a>Informes de Ingreso de Hardware<span class="fa fa-chevron-down"></span></a>
                         <ul class="nav child_menu">
-                            <li><a>Informes de Ingreso de Hardware<span class="fa fa-chevron-down"></span></a>
-                                <ul class="nav child_menu">
-                                    <li><a href="<?php echo SERVERURL; ?>informeingresohardware/">Aprobados</a>
-                                    </li>
-                                    <li><a href="<?php echo SERVERURL; ?>informeingresoechazado/">Rechazados</a>
-                                    </li>
-                                </ul>
+                            <li><a href="<?php echo SERVERURL; ?>informeingresohardware/">Aprobados</a>
                             </li>
-                            <li><a>Características<span class="fa fa-chevron-down"></span></a>
-                                <ul class="nav child_menu">
-                                    <li><a href="<?php echo SERVERURL; ?>tipohardware/">Tipos</a>
-                                    </li>
-                                    <li><a href="<?php echo SERVERURL; ?>marcashardware/">Marcas</a>
-                                    </li>
-                                    <li><a href="<?php echo SERVERURL; ?>modeloshardware/">Modelos</a>
-                                    </li>
-                                    <li><a href="<?php echo SERVERURL; ?>coloreshardware/">Colores</a>
-                                    </li>
-                                </ul>
+                            <li><a href="<?php echo SERVERURL; ?>informeingresoechazado/">Rechazados</a>
                             </li>
-                            <li><a href="<?php echo SERVERURL; ?>estadosHardwareingreso/">Estados de ingreso</a></li>
-                            <li><a href="<?php echo SERVERURL; ?>estadosHardware/">Estados de hardware</a></li>
-                            <li><a href="<?php echo SERVERURL; ?>estadosHardwareasignacion/">Estados de re/asignación</a></li>
                         </ul>
                     </li>
-
-                    <li><a><i class="fa fa-sign-out"></i> Re / Asignación <span class="fa fa-chevron-down"></span></a>
+                    <li><a>Características<span class="fa fa-chevron-down"></span></a>
                         <ul class="nav child_menu">
-                            <li><a>Asignación<span class="fa fa-chevron-down"></span></a>
-                                <ul class="nav child_menu">
-                                    <li><a href="<?php echo SERVERURL; ?>asignacion/">Sin asignar</a>
-                                    </li>
-                                    <li><a href="<?php echo SERVERURL; ?>asignados/">Asignados</a>
-                                    </li>
-                                </ul>
+                            <li><a href="<?php echo SERVERURL; ?>tipohardware/">Tipos</a>
                             </li>
-                            <li><a>Reasignación<span class="fa fa-chevron-down"></span></a>
-                                <ul class="nav child_menu">
-                                    <li><a href="<?php echo SERVERURL; ?>liberados/">Liberados</a>
-                                    </li>
-                                    <li><a href="<?php echo SERVERURL; ?>reasignados/">Reasignados</a>
-                                    </li>
-                                </ul>
+                            <li><a href="<?php echo SERVERURL; ?>marcashardware/">Marcas</a>
                             </li>
-
+                            <li><a href="<?php echo SERVERURL; ?>modeloshardware/">Modelos</a>
+                            </li>
+                            <li><a href="<?php echo SERVERURL; ?>coloreshardware/">Colores</a>
+                            </li>
                         </ul>
                     </li>
- 
-                    <li><a><i class="fa fa-bar-chart-o"></i> Mantenimiento <span class="fa fa-chevron-down"></span></a>
+                    <li><a href="<?php echo SERVERURL; ?>estadosHardwareingreso/">Estados de ingreso</a></li>
+                    <li><a href="<?php echo SERVERURL; ?>estadosHardware/">Estados de hardware</a></li>
+                    <li><a href="<?php echo SERVERURL; ?>estadosHardwareasignacion/">Estados de re/asignación</a></li>
+                </ul>
+            </li>
+
+            <li><a><i class="fa fa-sign-out"></i> Re / Asignación <span class="fa fa-chevron-down"></span></a>
+                <ul class="nav child_menu">
+                    <li><a>Asignación<span class="fa fa-chevron-down"></span></a>
                         <ul class="nav child_menu">
-                            <li><a>Gestionar Mantenimiento<span class="fa fa-chevron-down"></span></a>
-                                <ul class="nav child_menu">
-                                    <li><a href="<?php echo SERVERURL; ?>mantenimiento/">Hardware</a>
-                                    </li>
-                                    <li><a href="<?php echo SERVERURL; ?>reparacion/">En reparación</a>
-                                    </li>
-                                    <li><a href="<?php echo SERVERURL; ?>dadodebaja/">Dado de baja</a>
-                                    </li>
-                                </ul>
+                            <li><a href="<?php echo SERVERURL; ?>asignacion/">Sin asignar</a>
                             </li>
-                            <li><a href="<?php echo SERVERURL; ?>tiposMantenimiento/">Tipos de Mantenimiento</a></li>
-                            <li><a href="<?php echo SERVERURL; ?>estadosMantenimiento/">Estados de Mantenimiento</a></li>
+                            <li><a href="<?php echo SERVERURL; ?>asignados/">Asignados</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li><a>Reasignación<span class="fa fa-chevron-down"></span></a>
+                        <ul class="nav child_menu">
+                            <li><a href="<?php echo SERVERURL; ?>liberados/">Liberados</a>
+                            </li>
+                            <li><a href="<?php echo SERVERURL; ?>reasignados/">Reasignados</a>
+                            </li>
                         </ul>
                     </li>
 
-                   <li><a><i class="fa fa-user"></i> Administradores <span class="fa fa-chevron-down"></span></a>
-                    <ul class="nav child_menu">
-                        <li><a href="<?php echo SERVERURL; ?>administrador/">Administrador</a></li>
-                        <!--<li><a href="<?php echo SERVERURL; ?>perfil/">Perfil de usuario</a></li>-->
-                        <li><a href="<?php echo SERVERURL; ?>estadosAdministrador/">Estados del administrador</a></li>
-                    </ul>
-                </li>
+                </ul>
+            </li>
 
-                <li><a><i class="fa fa-windows"></i> Accesos <span class="fa fa-chevron-down"></span></a>
-                    <ul class="nav child_menu">
-                        <li><a href="<?php echo SERVERURL; ?>roles/">Roles</a></li>
-                        <li><a href="<?php echo SERVERURL; ?>modulos/">Modulos</a></li>
-                    </ul>
-                </li>
+            <li><a><i class="fa fa-bar-chart-o"></i> Mantenimiento <span class="fa fa-chevron-down"></span></a>
+                <ul class="nav child_menu">
+                    <li><a>Gestionar Mantenimiento<span class="fa fa-chevron-down"></span></a>
+                        <ul class="nav child_menu">
+                            <li><a href="<?php echo SERVERURL; ?>mantenimiento/">Hardware</a>
+                            </li>
+                            <li><a href="<?php echo SERVERURL; ?>reparacion/">En reparación</a>
+                            </li>
+                            <li><a href="<?php echo SERVERURL; ?>dadodebaja/">Dado de baja</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li><a href="<?php echo SERVERURL; ?>tiposMantenimiento/">Tipos de Mantenimiento</a></li>
+                    <li><a href="<?php echo SERVERURL; ?>estadosMantenimiento/">Estados de Mantenimiento</a></li>
+                </ul>
+            </li>
+
+            <li><a><i class="fa fa-user"></i> Administradores <span class="fa fa-chevron-down"></span></a>
+                <ul class="nav child_menu">
+                    <li><a href="<?php echo SERVERURL; ?>administrador/">Administrador</a></li>
+                    <!--<li><a href="<?php echo SERVERURL; ?>perfil/">Perfil de usuario</a></li>-->
+                    <li><a href="<?php echo SERVERURL; ?>estadosAdministrador/">Estados del administrador</a></li>
+                </ul>
+            </li>
+
+            <li><a><i class="fa fa-windows"></i> Accesos <span class="fa fa-chevron-down"></span></a>
+                <ul class="nav child_menu">
+                    <li><a href="<?php echo SERVERURL; ?>roles/">Roles</a></li>
+                    <li><a href="<?php echo SERVERURL; ?>modulos/">Modulos</a></li>
+                </ul>
+            </li>
 
 
+        </div>
     </div>
-</div>
